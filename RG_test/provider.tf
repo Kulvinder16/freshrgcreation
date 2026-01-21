5c767b8894dc6d098a2324354d15c92d60eb0a0a
Kulvinder Singh <kulvindersinghmatharoo@gmail.com>
2026-01-21 17:09:01 +0530

    Check project

---
+++ RG_test/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rgkulvinder"
+    storage_account_name = "stgkulvinder1"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "container1"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "0bc661e3-3fc6-4649-92e7-740d5c1311fb"
+}
